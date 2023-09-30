688e70b9d71b1cd5e025453b7fd316137dd5f521
GururajHDesai <desaigururaj14@gmail.com>
2023-09-30 03:50:13 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div>
+    
+    <footer className="bottom">
+        <p className="center">
+            All rights reserved &copy; GURURAJ HANAMESH DESAI
+        </p>
+    </footer>
+</div>
+  );
+}
+
+export default Footer;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import{
+    
+    Link
+  }from "react-router-dom";
+
+
+function Menu() {
+  return (
+    <nav className="menu"
+    role="navigation"
+    aria-label="Main menu"
+    itemScope
+    itemType='http://schema.org/SiteNavigationElement'
+    >
+    <ul>
+        
+        <li><Link itemProp="url" to="/">HomePage</Link></li>
+        <li><Link itemProp="url" to="/about">About</Link></li>
+        <li><Link itemProp="url" to="/login">Login</Link></li>
+        
+    </ul>
+</nav>
+  );
+}
+
+export default Menu;
